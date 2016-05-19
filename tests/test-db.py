da2d531a6d8ed3f4df18036e6f6afec678b87792
John Kitchin <jkitchin@cmu.edu>
2016-05-18 21:51:13 -0400

    new db test

---
+++ tests/test-db.py
@@ -0,0 +1,36 @@
+from nose import *
+from vasp import *
+import os
+from ase import Atom, Atoms
+from ase.db import connect
+def setup_func():
+    "set up test fixtures"
+    if os.path.exists('DB.db'):
+        #os.unlink('DB.db')
+        pass
+
+
+def teardown_func():
+    "tear down test fixtures"
+    if os.path.exists('DB.db'):
+        #os.unlink('DB.db')
+        pass
+
+@with_setup(setup_func, teardown_func)
+def test0():
+    atoms = Atoms([Atom('O', [4, 5, 5], magmom=1),
+                   Atom('C', [5, 5, 5], magmom=2),
+                   Atom('O', [6, 5, 5], magmom=3)],
+                   cell=(10, 10, 10))
+
+    calc = Vasp('vasp',
+                sigma=0.01,
+                atoms=atoms)
+
+    calc.write_db(fname='DB.db', append=False)
+
+    with connect('DB.db') as con:
+        data = con.get(1).data
+        assert data['parameters']['sigma'] == 0.01
+
+    print 'done'
