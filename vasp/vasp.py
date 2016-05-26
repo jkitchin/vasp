6acdcec79088ef46f768950c68de6084a6f97160
John Kitchin <jkitchin@cmu.edu>
2016-05-26 09:38:25 -0400

    update logging setup

--- vasp/vasp.py
+++ vasp/vasp.py
@@ -10,19 +10,11 @@
 import logging
 import sys
 
-log = logging.getLogger('Vasp')
-log.setLevel(logging.CRITICAL)
-handler = logging.StreamHandler()
-if sys.version_info < (2, 5):  # no funcName in python 2.4
-    formatstring = ('%(levelname)-10s '
-                    'lineno: %(lineno)-4d %(message)s')
-else:
-    formatstring = ('%(levelname)-10s function: %(funcName)s '
-                    'lineno: %(lineno)-4d %(message)s')
-formatter = logging.Formatter(formatstring)
-handler.setFormatter(formatter)
-log.addHandler(handler)
+logging.basicConfig(stream=sys.stdout,
+                    format=('%(levelname)-8s [[%(pathname)s::%(lineno)d][%(funcName)s]]: %(message)s'),
+                    level=logging.INFO)
 
+log = logging.getLogger('Vasp')
 
 # The core class is defined here
 from vasp_core import Vasp
